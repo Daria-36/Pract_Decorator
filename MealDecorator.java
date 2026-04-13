18210ac7c3743f7e51a876301c41eb3cf93ebe33
Daria-36 <darushckap@gmail.com>
2026-04-13 11:34:13 +0300

    Update MealDecorator.java

--- MealDecorator.java
+++ MealDecorator.java
@@ -2,10 +2,6 @@ package ru.university.decorator;
 
 import java.util.Objects;
 
-/**
- * Абстрактный декоратор из учебника GoF: внутри храним другое {@link Meal} и делегируем ему вызовы,
- * а в наследниках добавляем свою надбавку к цене и куски текста в название заказа.
- */
 public abstract class MealDecorator implements Meal {
 
     protected final Meal wrapped;
@@ -14,12 +10,10 @@ public abstract class MealDecorator implements Meal {
         this.wrapped = Objects.requireNonNull(wrapped, "Внутри декоратора не должно быть пустой ссылки на яство");
     }
 
-    @Override
     public int getPriceSeptims() {
         return wrapped.getPriceSeptims();
     }
 
-    @Override
     public String describeOrderLine() {
         return wrapped.describeOrderLine();
     }
