9c1d820ff0a3454d9627266fd5396e29499086cd
Daria-36 <darushckap@gmail.com>
2026-05-11 11:09:59 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,12 +8,6 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception ignored) {
-
-            }
             RunicStewFrame frame = new RunicStewFrame();
             frame.setVisible(true);
         });
