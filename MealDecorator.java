d754202a5fd4a5527a13cd1992f276ac7586757e
Daria-36 <darushckap@gmail.com>
2026-05-19 11:48:03 +0300

    Update MealDecorator.java

--- MealDecorator.java
+++ MealDecorator.java
@@ -4,9 +4,9 @@ import java.util.Objects;
 
 public abstract class MealDecorator implements Meal {
 
-    private final Meal wrapped;
+    private Meal wrapped;
 
-    protected MealDecorator(Meal wrapped) {
+    public MealDecorator(Meal wrapped) {
         this.wrapped = Objects.requireNonNull(wrapped, "Внутри декоратора не должно быть пустой ссылки на яство");
     }
 
