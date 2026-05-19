40d6c5864ac4d4c3b1a30de2cc028d7db274e141
Daria-36 <darushckap@gmail.com>
2026-05-19 12:40:23 +0300

    ModifierBinding.java

--- ModifierBinding.java
+++ ModifierBinding.java
@@ -1,29 +1,41 @@
 package ru.university.decorator;
 
-import javax.swing.*;
-import java.util.Map;
-import java.util.function.UnaryOperator;
+import javax.swing.JCheckBox;
 
 public final class ModifierBinding {
-    
+    public static final int FIRE_SAUCE = 1;
+    public static final int DOUBLE_VENISON = 2;
+    public static final int SNOW_BERRIES = 3;
+    public static final int NORDIC_FLATBREAD = 4;
+
     private final JCheckBox notch;
-    private final UnaryOperator<Meal> decorateSelected;
-    
-    public ModifierBinding(JCheckBox notch, UnaryOperator<Meal> decorateSelected) {
+    private final int modifierType;
+
+    public ModifierBinding(JCheckBox notch, int modifierType) {
         this.notch = notch;
-        this.decorateSelected = decorateSelected;
+        this.modifierType = modifierType;
     }
-    
+
     public JCheckBox notch() {
         return notch;
     }
-    
+
     public Meal fold(Meal base) {
         if (notch.isSelected()) {
-
-            return decorateSelected.apply(base);
-        } else {
-            return base;
+            switch (modifierType) {
+                case FIRE_SAUCE:
+                    return new FireSauceDecorator(base);
+                case DOUBLE_VENISON:
+                    return new DoubleVenisonDecorator(base);
+                case SNOW_BERRIES:
+                    return new SnowBerriesDecorator(base);
+                case NORDIC_FLATBREAD:
+                    return new NordicFlatbreadDecorator(base);
+                default:
+                    return base;
+            }
         }
+
+        return base;
     }
 }
